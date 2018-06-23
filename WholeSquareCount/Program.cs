0682ee8d2003250a6398b8424938461a2735b91d
Anastasia <anaaustin1775@gmail.com>
2018-06-23 21:47:43 +0300

    Find whole square values

---
+++ WholeSquareCount/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WholeSquareCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            do
+            {
+                Console.Clear();
+                var isValid = true;
+                Console.Write("a = ");
+                isValid = int.TryParse(Console.ReadLine(), out int a);
+                if (!isValid) return;
+                Console.Write("b = ");
+                isValid = int.TryParse(Console.ReadLine(), out int b);
+                if (!isValid) return;
+                var result = solution.solution(a, b);
+                Console.WriteLine(string.Join(", ", Array.ConvertAll(result,x => x.ToString())));
+                Console.Write("Repeat? (y/n)");
+
+            } while (ShouldRepeat());
+
+        }
+
+        static bool ShouldRepeat()
+        {
+            return Console.ReadKey(false).Key == ConsoleKey.Y;
+        }
+    }
+}
---
+++ WholeSquareCount/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WholeSquareCount
+{
+    public class Solution
+    {
+        public int[] solution(int a, int b)
+        {
+            var result = new List<int>();
+            if (!(a < 0 && b < 0))
+            {
+                var start = PreventNegative(a);
+                var end = PreventNegative(b);
+                for (int i = start; i <= end; i++)
+                {
+                    var root = Math.Sqrt(i);
+                    if (root % 1 == 0)
+                    {
+                        result.Add(i);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        private int PreventNegative(int value)
+        {
+            return value > 0 ? value : 0;
+        }
+    }
+}
